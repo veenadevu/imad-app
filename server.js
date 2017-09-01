318a08be1a778c20bfa8e6e794c2e6b53f34ea5d
veenadevu <30698049+veenadevu@users.noreply.github.com>
2017-09-01 20:34:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config = {
+    user: 'veenarema19',
+    database: 'veenarema19',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config);
+app.get('/test-db', function (req, res) {
+    
+    pool.Query('SELECT *FROM test', function(err,result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
